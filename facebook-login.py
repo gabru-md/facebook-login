db14d3af1f7aa141e4a0f760b720e760756301a6
Manish Devgan <manish.nsit8@gmail.com>
2017-09-18 21:18:52 +0100

    Update facebook-login.py

--- facebook-login.py
+++ facebook-login.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 def test(username, pass1):
 	browser=webdriver.PhantomJS() # this will be the path to your webbrowser's WEBDRIVER
-	browser.get('http://facebook.com')
+	browser.get('https://google.co.in')
 	time.sleep(10)
 	#user credentials
 	lst = browser.find_element_by_css_selector('#lst-ib')
